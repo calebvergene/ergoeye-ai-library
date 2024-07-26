071de0e17096d99fd1e516d7e887a99c377fe99d
caleb vergene <calebverg@gmail.com>
2024-07-26 13:56:28 -0700

    video connected with opencv

--- venv/main.py
+++ venv/main.py
@@ -1,2 +1,13 @@
 import cv2
 import mediapipe as mp
+
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture('PoseVideos/1.mp4')
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(40)
