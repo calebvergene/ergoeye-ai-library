be6a79f2399ad5b28d2f00d70b66f623a01c34f6
caleb vergene <calebverg@gmail.com>
2024-07-26 20:40:26 -0700

    file organize

--- venv/main.py
+++ venv/main.py
@@ -1,50 +1,6 @@
 import cv2
 import mediapipe as mp
-
-
-class poseDetector():
-    def __init__(self,static_image_mode=False, model_complexity=1, smooth_landmarks=True, enable_segmentation=False,
-                smooth_segmentation=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        
-        self.static_image_mode=False
-        self.model_complexity=1
-        self.smooth_landmarks=True
-        self.enable_segmentation=False
-        self.smooth_segmentation=True
-        self.min_detection_confidence=0.5
-        self.min_tracking_confidence=0.5
-
-        # Initialize drawing and pose utilities from MediaPipe
-        self.mpDraw = mp.solutions.drawing_utils
-        self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.static_image_mode, self.model_complexity, self.smooth_landmarks, self.enable_segmentation, self.smooth_segmentation, self.min_detection_confidence, self.min_tracking_confidence)
-
-    
-    def find_pose(self, img, draw=True):
-
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        self.results = self.pose.process(imgRGB)
-        ## Draws data from joint points on body in live video
-        if self.results.pose_landmarks:
-            if draw:
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
-        return img
-
-    def find_position(self, img, draw=True):
-        landmark_list = []
-
-        if self.results.pose_landmarks:
-            for id, landmark in enumerate(self.results.pose_landmarks.landmark):
-                height, width, channel = img.shape
-                # convert to pixel value
-                cx, cy = int(landmark.x * width), int(landmark.y * height)
-                landmark_list.append([id, cx, cy])
-                cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
-
-        return landmark_list
-
-        
-
+from pose_module import poseDetector
 
 def main():
     # Creates video object
@@ -66,7 +22,7 @@ def main():
         
         img = detector.find_pose(img)
         landmark_list = detector.find_position(img)
-        print(landmark_list)
+        
     
         cv2.imshow("Image", img)
         cv2.waitKey(20)
--- venv/pose_module.py
+++ venv/pose_module.py
@@ -1,40 +1,44 @@
 import cv2
 import mediapipe as mp
 
-# Initialize drawing and pose utilities from MediaPipe
-mpDraw = mp.solutions.drawing_utils
-mpPose = mp.solutions.pose
-pose = mpPose.Pose()
 
-# Creates video object
-cap = cv2.VideoCapture('PoseVideos/1.mp4')
+class poseDetector():
+    def __init__(self,static_image_mode=False, model_complexity=1, smooth_landmarks=True, enable_segmentation=False,
+                smooth_segmentation=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        
+        self.static_image_mode=False
+        self.model_complexity=1
+        self.smooth_landmarks=True
+        self.enable_segmentation=False
+        self.smooth_segmentation=True
+        self.min_detection_confidence=0.5
+        self.min_tracking_confidence=0.5
 
+        # Initialize drawing and pose utilities from MediaPipe
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.static_image_mode, self.model_complexity, self.smooth_landmarks, self.enable_segmentation, self.smooth_segmentation, self.min_detection_confidence, self.min_tracking_confidence)
 
-## Processes image frames
-while True:
-    success, img = cap.read()
     
-    # Break the loop if the video ends
-    if not success:
-        print("Finished processing video.")
-        break
+    def find_pose(self, img, draw=True):
 
-    if img is None:
-        print("Warning: Captured frame is None.")
-        continue
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        self.results = self.pose.process(imgRGB)
+        ## Draws data from joint points on body in live video
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
 
+    def find_position(self, img, draw=True):
+        landmark_list = []
 
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    results = pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            for id, landmark in enumerate(self.results.pose_landmarks.landmark):
+                height, width, channel = img.shape
+                # convert to pixel value
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+                landmark_list.append([id, cx, cy])
+                cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
 
-    ## Draws data from joint points on body in live video
-    if results.pose_landmarks:
-        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-        for id, landmark in enumerate(results.pose_landmarks.landmark):
-            height, width, channel = img.shape
-            # convert to pixel value
-            cx, cy = int(landmark.x * width), int(landmark.y * height)
-            cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(20)
+        return landmark_list
